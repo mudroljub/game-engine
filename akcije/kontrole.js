96c44316472548d198936e0aa1c6e601c8a54f72
Damjan Pavlica <mudroljub@gmail.com>
2017-02-08 01:12:05 +0100

    tipke

--- akcije/kontrole.js
+++ akcije/kontrole.js
@@ -1,13 +1,22 @@
 import tipke from 'io/tipke'
 import * as $ from 'konstante'
 
+let pripremi = false
+
 // zahteva da akter ima implementirane metode
 export function proveriTipke(akter) {
   if (tipke.stisnute[$.A]) akter.nalevo()
   if (tipke.stisnute[$.D]) akter.nadesno()
   if (tipke.stisnute[$.W]) akter.nagore()
   if (tipke.stisnute[$.S]) akter.nadole()
-  if (tipke.stisnute[$.RAZMAK]) akter.pucaj()
+//  if (tipke.stisnute[$.RAZMAK]) akter.pucaj()
+	if (tipke.stisnute[$.RAZMAK]) {
+		pripremi = true
+	}	
+	if (pripremi && !tipke.stisnute[$.RAZMAK]) {
+		akter.pucaj()
+		pripremi = false
+	}	
 }
 
 export function proveriTipke2(akter) {
